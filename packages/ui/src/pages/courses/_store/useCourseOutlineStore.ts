ff1a6f7df111b03c935aef3b86429d78ed1ad7a2
giaonnq-gfi <giaonnq@gfigroup.io>
2024-12-16 14:30:43 +0700

    feat: add course outline store #4

---
+++ packages/ui/src/pages/courses/_store/useCourseOutlineStore.ts
@@ -0,0 +1,61 @@
+import type { ICourseFile, ICoursePreviewVideo } from '@oe/api/types/course/basic';
+import type { ICourseOutline } from '@oe/api/types/course/course';
+import { create } from 'zustand';
+
+export interface PreviewVideo extends ICourseFile {
+  title: string;
+}
+
+interface CourseOutlineDetailState {
+  courseOutline: ICourseOutline;
+  setCourseOutline: (courseOutline?: ICourseOutline) => void;
+  getPreviewLessonVideo: () => PreviewVideo[] | undefined;
+}
+
+// Helper functions
+const mergeCourseOutline = (currentOutline: ICourseOutline, newOutline?: ICourseOutline): ICourseOutline => {
+  if (!newOutline) {
+    return currentOutline;
+  }
+
+  return {
+    ...currentOutline,
+    ...newOutline,
+    thumbnail: newOutline.thumbnail ?? currentOutline.thumbnail,
+    is_paid: newOutline.is_paid ?? currentOutline.is_paid,
+    is_enrolled: newOutline.is_enrolled ?? currentOutline.is_enrolled,
+  };
+};
+
+const mapMediaToPreviewVideo = (media: ICourseFile, previewLessons?: ICoursePreviewVideo[]): PreviewVideo => {
+  const matchingLesson = previewLessons?.find(lesson => lesson?.file_id === media.id);
+
+  return matchingLesson
+    ? {
+        ...media,
+        title: matchingLesson.title,
+      }
+    : { ...media, title: '' };
+};
+
+// Store
+export const useCourseOutlineDetailStore = create<CourseOutlineDetailState>()((set, get) => ({
+  courseOutline: {} as ICourseOutline,
+
+  setCourseOutline(newOutline?: ICourseOutline) {
+    set(state => ({
+      courseOutline: mergeCourseOutline(state.courseOutline, newOutline),
+    }));
+  },
+
+  getPreviewLessonVideo() {
+    const { courseOutline } = get();
+    const { medias, props } = courseOutline;
+
+    if (!medias) {
+      return undefined;
+    }
+
+    return medias.map(media => mapMediaToPreviewVideo(media, props?.preview_lessons ?? undefined));
+  },
+}));
