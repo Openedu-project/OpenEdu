59b92f4273b5743c25931c75e024699b20fb1774
phuongle-vbi <phuongltk@gfigroup.io>
2024-12-31 10:38:10 +0700

    feat: create blog breadcrumb #14

--- packages/ui/src/common/pages/index.ts
+++ packages/ui/src/common/pages/index.ts
@@ -1,4 +1,4 @@
-import BlogCreationPage from './blog/creation';
+import BlogCreationPage from './blog/blog-creation';
 import ErrorPage from './error';
 import NotFoundPage from './not-found';
 import OrgNotFoundPage from './org-not-found';
