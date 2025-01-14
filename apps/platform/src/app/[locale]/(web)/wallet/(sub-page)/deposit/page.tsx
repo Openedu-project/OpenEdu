19e7b3b8377da393e82d1a3ff0aa275749f528b3
tuongvbi <tuongtpn@gfigroup.io>
2025-01-14 15:41:09 +0700

    feat: enhance WalletDepositPage with DepositHeader component and improved layout

--- apps/platform/src/app/[locale]/(web)/wallet/(sub-page)/deposit/page.tsx
+++ apps/platform/src/app/[locale]/(web)/wallet/(sub-page)/deposit/page.tsx
@@ -1,5 +1,5 @@
+import { Deposit, DepositHeader } from '@oe/ui/common/wallet/deposit';
 import type { Metadata } from 'next';
-import Deposit from './_components/deposit';
 
 export const metadata: Metadata = {
   title: 'Wallet - Deposit',
@@ -7,7 +7,10 @@ export const metadata: Metadata = {
 
 const WalletDepositPage = () => (
   <main className="w-full max-w-lg mx-auto md:p-6">
-    <Deposit />
+    <div className="rounded-2xl bg-white p-4 shadow-[0px_4px_30px_0px_#F4F5F6] sm:p-8 space-y-6">
+      <DepositHeader />
+      <Deposit />
+    </div>
   </main>
 );
 
