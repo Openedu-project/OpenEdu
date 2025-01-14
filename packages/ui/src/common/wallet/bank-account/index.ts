2e5624bb2ea88913e9bbd7e00ed88773aaca8d06
tuongvbi <tuongtpn@gfigroup.io>
2025-01-14 16:00:57 +0700

    feat: create index file for bank account components to streamline exports

---
+++ packages/ui/src/common/wallet/bank-account/index.ts
@@ -0,0 +1,4 @@
+import BankAccountDialog, { BackAccountDialogTrigger } from './bank-account-dialog';
+import BankAccountList from './bank-account-list';
+
+export { BankAccountDialog, BackAccountDialogTrigger, BankAccountList };
