3371d04561e19108d30249b05654043304d9e588
tuongvbi <tuongtpn@gfigroup.io>
2025-01-14 15:39:58 +0700

    feat: enhance BankAccountsPage with localization and new components

--- apps/platform/src/app/[locale]/(web)/wallet/(sub-page)/bank-accounts/page.tsx
+++ apps/platform/src/app/[locale]/(web)/wallet/(sub-page)/bank-accounts/page.tsx
@@ -1,11 +1,28 @@
 import type { Metadata } from 'next';
 
-import BankAccountsPage from './_components/bank-accounts';
+import { BackAccountDialogTrigger, BankAccountDialog, BankAccountList } from '@oe/ui/common/wallet/bank-account';
+import { WalletBackButton } from '@oe/ui/common/wallet/shared';
+import { useTranslations } from 'next-intl';
 
 export const metadata: Metadata = {
   title: 'Wallet - Bank Accounts',
 };
 
-const WalletBankAccountsPage = () => <BankAccountsPage />;
+const WalletBankAccountsPage = () => {
+  const t = useTranslations('bankAccountPage');
+  return (
+    <div className="min-h-screen space-y-6">
+      <div className="flex justify-between gap-2">
+        <div className="flex gap-2 items-center">
+          <WalletBackButton />
+          <h2 className="text-[16px] md:text-[24px] font-semibold leading-9 m-0">{t('bankAccounts')}</h2>
+        </div>
+        <BackAccountDialogTrigger />
+      </div>
+      <BankAccountList />
+      <BankAccountDialog />
+    </div>
+  );
+};
 
 export default WalletBankAccountsPage;
