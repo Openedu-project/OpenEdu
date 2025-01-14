f097f5cc7dcabf17a0d86b4d492fd5bd0f2ce03a
tuongvbi <tuongtpn@gfigroup.io>
2025-01-14 17:09:00 +0700

    fix: update WalletBackButton to navigate to wallet page instead of going back

--- packages/ui/src/common/wallet/shared/wallet-back-button.tsx
+++ packages/ui/src/common/wallet/shared/wallet-back-button.tsx
@@ -1,18 +1,15 @@
-"use client";
+'use client';
 
-import { useRouter } from "#common/navigation";
-import { Button } from "#shadcn/button";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft } from 'lucide-react';
+import { useRouter } from '#common/navigation';
+import { Button } from '#shadcn/button';
 
 const WalletBackButton = () => {
   const router = useRouter();
 
   return (
-    <Button variant="ghost" className="p-2" onClick={() => router.back()}>
-      <ChevronLeft
-        color="#6368DC"
-        className="cursor-pointer transition-all hover:scale-110 active:scale-110"
-      />
+    <Button variant="ghost" className="p-2" onClick={() => router.push('/wallet')}>
+      <ChevronLeft color="#6368DC" className="cursor-pointer transition-all hover:scale-110 active:scale-110" />
     </Button>
   );
 };
