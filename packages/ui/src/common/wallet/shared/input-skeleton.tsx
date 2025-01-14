2ce9712820af6acde240aee6ac4920599ceb729b
tuongvbi <tuongtpn@gfigroup.io>
2025-01-14 17:07:24 +0700

    feat: add InputSkeleton component for loading state representation in wallet UI

---
+++ packages/ui/src/common/wallet/shared/input-skeleton.tsx
@@ -0,0 +1,9 @@
+const InputSkeleton = () => {
+  return (
+    <div className="animate-pulse">
+      <div className="h-14 w-full rounded-lg bg-gray-200" />
+    </div>
+  );
+};
+
+export default InputSkeleton;
