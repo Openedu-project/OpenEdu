2f149d6737c065c9bdc50694cabb0fc6718fcb30
linhdatran <datl@gfigroup.io>
2025-02-11 11:17:47 +0700

    feat: switch org_id to domain at featured content fe#2

--- packages/themes/src/avail/homepage/_actions/blog.ts
+++ packages/themes/src/avail/homepage/_actions/blog.ts
@@ -1,12 +1,10 @@
 import { getPopularBlogsServicesAtWebsite } from '@oe/api/services/featured-contents';
-import { getOrgByDomainService } from '@oe/api/services/organizations';
 import { getCookie } from '@oe/core/utils/cookie';
 export const getPopularBlogs = async () => {
   const domain = (await getCookie(process.env.NEXT_PUBLIC_COOKIE_API_REFERRER_KEY)) ?? '';
   try {
-    const orgData = await getOrgByDomainService(undefined, { domain });
     const dataPopularBlogs = await getPopularBlogsServicesAtWebsite(undefined, {
-      params: { org_id: orgData?.id ?? '' },
+      params: { org_id: domain ?? '' },
     });
     return dataPopularBlogs;
   } catch (error) {
--- packages/themes/src/vbi/_components/blog-grid-client.tsx
+++ packages/themes/src/vbi/_components/blog-grid-client.tsx
@@ -1,20 +1,18 @@
-'use client';
-import { useGetListBlogs } from '@oe/api/hooks/useBlog';
-import { formatDate } from '@oe/core/utils/datetime';
-import { Link } from '@oe/ui/common/navigation';
-import { Image } from '@oe/ui/components/image';
+"use client";
+import { useGetPopularContentsAtWebsite } from "@oe/api/hooks/useFeaturedContent";
+import type { IBlog } from "@oe/api/types/blog";
+import { getAPIReferrerAndOriginClient } from "@oe/api/utils/referrer-origin";
+import { formatDate } from "@oe/core/utils/datetime";
+import { Link } from "@oe/ui/common/navigation";
+import { Image } from "@oe/ui/components/image";
 const BlogGridClient = () => {
-  const { blogsData } = useGetListBlogs({
-    params: {
-      per_page: 4,
-      page: 1,
-      sort: 'create_at desc',
-      is_active: true,
-    },
+  const { host } = getAPIReferrerAndOriginClient();
+  const { dataPopularContents } = useGetPopularContentsAtWebsite<IBlog>({
+    params: { org_id: host ?? "", entity_type: "blog" },
   });
 
-  const featuredPost = blogsData?.results?.[0];
-  const restPost = blogsData?.results?.slice(1);
+  const featuredPost = dataPopularContents?.[0]?.entity;
+  const restPost = dataPopularContents?.slice(1, 3)?.map((v) => v?.entity);
 
   return (
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-8">
@@ -27,14 +25,16 @@ const BlogGridClient = () => {
           <div className="w-full overflow-hidden rounded-3xl">
             <Image
               src={featuredPost?.banner?.url}
-              alt={featuredPost?.title ?? ''}
+              alt={featuredPost?.title ?? ""}
               width={600}
               height={350}
               className="h-[270px] w-full object-cover"
             />
           </div>
           <div className="flex flex-col">
-            <div className="mcaption-regular14">{formatDate(featuredPost?.create_at ?? 0)}</div>
+            <div className="mcaption-regular14">
+              {formatDate(featuredPost?.create_at ?? 0)}
+            </div>
 
             <h3 className="giant-iheading-semibold20 md:giant-iheading-semibold24 hover:text-primary">
               {featuredPost?.title}
@@ -44,20 +44,22 @@ const BlogGridClient = () => {
               <div className="flex items-center justify-center rounded-full">
                 <Image
                   src={featuredPost?.author?.avatar}
-                  alt={featuredPost?.author?.display_name ?? ''}
+                  alt={featuredPost?.author?.display_name ?? ""}
                   width={24}
                   height={24}
                   className="h-[28px] w-[28px] rounded-full"
                 />
               </div>
-              <span className="mbutton-semibold16">{featuredPost?.author?.display_name}</span>
+              <span className="mbutton-semibold16">
+                {featuredPost?.author?.display_name}
+              </span>
             </div>
 
             <div
               className="mcaption-regular14 line-clamp-5"
               // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
               dangerouslySetInnerHTML={{
-                __html: featuredPost?.content ?? '',
+                __html: featuredPost?.content ?? "",
               }}
             />
           </div>
@@ -66,45 +68,57 @@ const BlogGridClient = () => {
 
       {/* Other Posts */}
       <div className="space-y-8 lg:w-full">
-        {restPost?.map(post => (
-          <div key={post.id} className="group rounded-3xl bg-white shadow-[0px_4px_30px_0px_rgba(175,175,175,0.20)] ">
-            <Link
-              href={`/blog/${post?.id}`}
-              className="flex h-auto flex-col items-start gap-6 whitespace-break-spaces text-black no-underline hover:no-underline md:flex-row md:items-center"
-            >
-              <div className="w-full overflow-hidden rounded-2xl md:w-[260px]">
-                <Image
-                  src={post?.banner?.url}
-                  alt={post?.title}
-                  width={260}
-                  height={152}
-                  className="h-[152px] object-cover"
-                />
-              </div>
-
-              <div className="min-w-0 flex-1">
-                <div className="mcaption-regular14 mb-2">{formatDate(post?.create_at)}</div>
-                <h3 className="giant-iheading-semibold20 mb-3 line-clamp-2">{post?.title}</h3>
-                <div className="flex items-center gap-3">
-                  <div className="flex items-center justify-center rounded-full">
+        {restPost?.map(
+          (post) =>
+            post && (
+              <div
+                key={post?.id}
+                className="group rounded-3xl bg-white shadow-[0px_4px_30px_0px_rgba(175,175,175,0.20)] "
+              >
+                <Link
+                  href={`/blog/${post?.id}`}
+                  className="flex h-auto flex-col items-start gap-6 whitespace-break-spaces text-black no-underline hover:no-underline md:flex-row md:items-center"
+                >
+                  <div className="w-full overflow-hidden rounded-2xl md:w-[260px]">
                     <Image
-                      src={post?.author?.avatar}
-                      alt={post?.author?.display_name}
-                      width={24}
-                      height={24}
-                      className="h-[28px] w-[28px] rounded-full"
+                      src={post?.banner?.url}
+                      alt={post?.title || ""}
+                      width={260}
+                      height={152}
+                      className="h-[152px] object-cover"
                     />
                   </div>
-                  <span className="mbutton-semibold16">{post?.author?.display_name}</span>
-                </div>
+
+                  <div className="min-w-0 flex-1">
+                    <div className="mcaption-regular14 mb-2">
+                      {formatDate(post?.create_at || 0)}
+                    </div>
+                    <h3 className="giant-iheading-semibold20 mb-3 line-clamp-2">
+                      {post?.title}
+                    </h3>
+                    <div className="flex items-center gap-3">
+                      <div className="flex items-center justify-center rounded-full">
+                        <Image
+                          src={post?.author?.avatar}
+                          alt={post?.author?.display_name || ""}
+                          width={24}
+                          height={24}
+                          className="h-[28px] w-[28px] rounded-full"
+                        />
+                      </div>
+                      <span className="mbutton-semibold16">
+                        {post?.author?.display_name}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          </div>
-        ))}
+            )
+        )}
       </div>
     </div>
   );
 };
 
-BlogGridClient.displayName = 'BlogGridClient';
+BlogGridClient.displayName = "BlogGridClient";
 export { BlogGridClient };
