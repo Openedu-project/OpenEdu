9792f4f07e6457c254ec9e4e3f888acd670ce364
linhdatran <datl@gfigroup.io>
2025-03-21 16:37:53 +0700

    fix: adjust theme-system key

--- packages/api/src/hooks/useTheme.ts
+++ packages/api/src/hooks/useTheme.ts
@@ -1,13 +1,14 @@
 import useSWR from 'swr';
-import { getThemeConfigClient } from '#services/theme';
+import { createOrUpdateThemeConfig, getThemeConfigClient } from '#services/theme';
 import type { ISystemConfigRes } from '#types/system-config';
 import { API_ENDPOINT } from '#utils/endpoints';
 import { createAPIUrl } from '#utils/fetch';
-import { getAPIReferrerAndOriginClient } from '#utils/referrer-origin';
 import { createThemeSystemConfigKeyClient } from '#utils/system-config';
 
-import type { ThemeName, ThemeSystem } from '../../../themes/src/_types';
+import type { ThemeSystem } from '../../../themes/src/_types';
 
+import useSWRMutation from 'swr/mutation';
+import type { IThemeSystemConfigPayload } from '#types/theme';
 import { useGetOrganizationByDomain } from './useOrganization';
 
 // export function useGetTheme(shouldFetch = true) {
@@ -26,17 +27,14 @@ import { useGetOrganizationByDomain } from './useOrganization';
 
 export function useGetTheme(fallback: ISystemConfigRes<ThemeSystem>[] | undefined = undefined) {
   const { organizationByDomain } = useGetOrganizationByDomain();
-  const { host } = getAPIReferrerAndOriginClient();
 
   const endpoint = createAPIUrl({
     endpoint: API_ENDPOINT.SYSTEM_CONFIGS,
     queryParams: {
-      keys: createThemeSystemConfigKeyClient(organizationByDomain?.domain?.split('.')?.[0] as ThemeName | undefined),
-      domains: host,
+      keys: createThemeSystemConfigKeyClient(organizationByDomain?.domain),
+      domains: organizationByDomain?.domain,
     },
   });
-
-  console.info('host', host);
 
   const { data, isLoading, error, mutate } = useSWR(endpoint, (endpoint: string) => getThemeConfigClient(endpoint), {
     fallbackData: fallback,
@@ -47,5 +45,27 @@ export function useGetTheme(fallback: ISystemConfigRes<ThemeSystem>[] | undefine
     themeError: error,
     mutateTheme: mutate,
     themeLoading: isLoading,
+  };
+}
+
+export function useCreateOrUpdateThemeConfig() {
+  const { organizationByDomain } = useGetOrganizationByDomain();
+
+  const createConfigEndpoint = API_ENDPOINT.ADMIN_SYSTEM_CONFIGS;
+  const updateConfigEdnpoint = API_ENDPOINT.ADMIN_SYSTEM_CONFIGS_ID;
+
+  const { trigger, isMutating, error } = useSWRMutation(
+    createConfigEndpoint,
+    async (url: string, { arg }: { arg: IThemeSystemConfigPayload }) =>
+      createOrUpdateThemeConfig(arg?.id ? updateConfigEdnpoint : url, {
+        ...arg,
+        domain: organizationByDomain?.domain ?? arg.domain,
+      })
+  );
+
+  return {
+    createOrUpdateThemeConfig: trigger,
+    isLoadingCreateOrUpdateThemeConfig: isMutating,
+    errorCreateOrUpdateThemeConfig: error,
   };
 }
