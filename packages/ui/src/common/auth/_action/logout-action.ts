f6dbbc119d2fca7f9f531dc3c23bbfa001d50196
Duc Vuong <ducva@gfigroup.io>
2025-03-25 10:40:57 +0700

    fix: fix reload issue

--- packages/ui/src/common/auth/_action/logout-action.ts
+++ packages/ui/src/common/auth/_action/logout-action.ts
@@ -2,12 +2,11 @@
 
 import { setCookie } from '@oe/core/utils/cookie';
 import { PLATFORM_ROUTES, isProtectedRoute } from '@oe/core/utils/routes';
-import { revalidatePath } from 'next/cache';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export async function logoutAction() {
-  revalidatePath('/', 'layout');
+  // revalidatePath('/', 'layout');
   await Promise.all([
     setCookie(process.env.NEXT_PUBLIC_COOKIE_ACCESS_TOKEN_KEY, '', { maxAge: 0 }),
     setCookie(process.env.NEXT_PUBLIC_COOKIE_REFRESH_TOKEN_KEY, '', { maxAge: 0 }),
--- packages/ui/src/common/navigation/link.tsx
+++ packages/ui/src/common/navigation/link.tsx
@@ -65,7 +65,7 @@ export const Link: FC<LinkProps> = ({
   }
 
   return (
-    <NextLink prefetch={true} {...linkProps}>
+    <NextLink prefetch={false} {...linkProps}>
       {children}
     </NextLink>
   );
