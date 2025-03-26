8225e08dd166e8197a72919548ef53291ce41481
Duc Vuong <ducva@gfigroup.io>
2025-03-26 11:10:35 +0700

    feat: change homepage component

--- apps/platform/src/app/[locale]/(web)/_components/creators.tsx
+++ apps/platform/src/app/[locale]/(web)/_components/creators.tsx
@@ -1,10 +1,10 @@
-import CreatorImage from "@oe/assets/images/openedu-homepage/banner-creators.png";
-import { Image } from "@oe/ui/components/image";
-import { Button } from "@oe/ui/shadcn/button";
-import { useTranslations } from "next-intl";
+import CreatorImage from '@oe/assets/images/openedu-homepage/banner-creators.svg';
+import { Image } from '@oe/ui/components/image';
+import { Button } from '@oe/ui/shadcn/button';
+import { useTranslations } from 'next-intl';
 
 export default function CreatorsSection() {
-  const t = useTranslations("homePageLayout.creatorsSection");
+  const t = useTranslations('homePageLayout.creatorsSection');
 
   return (
     <div className="relative w-full overflow-hidden">
@@ -12,21 +12,21 @@ export default function CreatorsSection() {
         <div className="flex flex-col items-center">
           <div className="mt-6 text-center md:mt-8 lg:mt-10">
             <h2 className="giant-iheading-bold20 md:giant-iheading-bold24 lg:giant-iheading-bold32 mb-4 md:mb-6">
-              {t("title")}
+              {t('title')}
             </h2>
             <p className="mcaption-regular16 lg:mcaption-regular24 mb-4 text-muted-foreground md:mb-6">
-              {t("description")}
+              {t('description')}
             </p>
             <Button size="lg" className="mbutton-bold16 w-full px-8 sm:w-fit">
-              {t("buttons.become")}
+              {t('buttons.become')}
             </Button>
           </div>
           <div className="mt-8 lg:mt-12">
-            <div className="relative flex w-full md:w-64 lg:w-[850px]">
+            <div className="relative flex w-full md:w-64 lg:w-[720px]">
               <Image
                 src={CreatorImage.src}
                 alt="OpenEdu Creator"
-                width={944}
+                width={800}
                 height={800}
                 className="rounded-full object-contain"
                 priority
