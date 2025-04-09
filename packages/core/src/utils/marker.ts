caf3f7bed90d23a02a7fc4cb9d9d97da68b070c5
phuongle <phuongltk@gfigroup.io>
2025-04-09 13:13:33 +0700

    fix: css for code block

--- packages/core/src/utils/marker.ts
+++ packages/core/src/utils/marker.ts
@@ -164,7 +164,7 @@ export const marked = new Marked({
         }, 2000);
       });
     })(this)"
-    data-copy-button data-code="${safeEncodeURIComponent(originalCode)}" class="flex items-center justify-center h-6 w-6 border rounded-sm hover:bg-gray-100">
+    data-copy-button data-code="${safeEncodeURIComponent(originalCode)}" class="flex items-center justify-center h-6 w-6 border rounded-sm hover:opacity-50">
     <svg class="copy-icon" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
       <path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.912 4.895 3 6 3h8c1.105 0 2 .912 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.088 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"/>
     </svg>
