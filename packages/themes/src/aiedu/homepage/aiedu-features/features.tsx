7ce0e323491e8f973cadcf412c96ddb60450cde7
linhdatran <datl@gfigroup.io>
2025-05-02 12:22:51 +0700

    fix: update UI #306

--- packages/themes/src/aiedu/homepage/aiedu-features/features.tsx
+++ packages/themes/src/aiedu/homepage/aiedu-features/features.tsx
@@ -1,11 +1,14 @@
-import { cn } from '@oe/ui';
-import { useTranslations } from 'next-intl';
+import { cn } from "@oe/ui";
+import { useTranslations } from "next-intl";
 
-import AICourseBg from '@oe/assets/images/theme/aiedu/ai-course.png';
-import { Image } from '@oe/ui';
-import type { SectionComponent } from '../../../_types/theme-page';
-import { AieduCourseCard, type AieduCourseCardProps } from '../../_components/course-card';
-import { AieduLayoutSection } from '../../_components/layout-section';
+import AICourseBg from "@oe/assets/images/theme/aiedu/ai-course.png";
+import { Image } from "@oe/ui";
+import type { SectionComponent } from "../../../_types/theme-page";
+import {
+  AieduCourseCard,
+  type AieduCourseCardProps,
+} from "../../_components/course-card";
+import { AieduLayoutSection } from "../../_components/layout-section";
 
 // Theme step 4: create sectionProps
 export interface AieduHomepageFeaturesProps {
@@ -18,13 +21,16 @@ export interface AieduHomepageFeaturesProps {
 }
 
 //Theme step 7: back to section - hero.tsx, create your code based on the props
-const AieduHomepageFeatures: SectionComponent<'homepage', 'aieduFeatures'> = ({ className, props }) => {
-  const t = useTranslations('themePage.aiedu.homepage.aieduFeatures');
+const AieduHomepageFeatures: SectionComponent<"homepage", "aieduFeatures"> = ({
+  className,
+  props,
+}) => {
+  const t = useTranslations("themePage.aiedu.homepage.aieduFeatures");
 
   return (
     <AieduLayoutSection
       background="bg-gradient-to-b from-primary from-50% to-white to-98%"
-      className={cn('space-y-8', className)}
+      className={cn("space-y-8", className)}
     >
       <Image
         src={AICourseBg.src}
@@ -39,25 +45,27 @@ const AieduHomepageFeatures: SectionComponent<'homepage', 'aieduFeatures'> = ({ 
       <div className="space-y-4 md:flex md:gap-12 md:space-y-0">
         <AieduCourseCard
           key="main"
-          title={t('mainCourse.title')}
-          benefits={Array.from({ length: props?.subCourse?.benefits?.length ?? 0 }, (_, i) =>
-            t(`mainCourse.benefits.benefit${i + 1}`)
+          title={t("mainCourse.title")}
+          benefits={Array.from(
+            { length: props?.subCourse?.benefits?.length ?? 0 },
+            (_, i) => t(`mainCourse.benefits.benefit${i + 1}`)
           )}
           image={props?.mainCourse?.image}
           button={{
-            text: t('mainCourse.button.text'),
+            text: t("mainCourse.button.text"),
             link: props?.mainCourse?.button?.link,
           }}
         />
         <AieduCourseCard
           key="sub"
-          title={t('subCourse.title')}
-          benefits={Array.from({ length: props?.subCourse?.benefits?.length ?? 0 }, (_, i) =>
-            t(`subCourse.benefits.benefit${i + 1}`)
+          title={t("subCourse.title")}
+          benefits={Array.from(
+            { length: props?.subCourse?.benefits?.length ?? 0 },
+            (_, i) => t(`subCourse.benefits.benefit${i + 1}`)
           )}
           image={props?.subCourse?.image}
           button={{
-            text: t('subCourse.button.text'),
+            text: t("subCourse.button.text"),
             link: props?.subCourse?.button?.link,
           }}
         />
--- packages/themes/src/aiedu/introduction/aiedu-trend/trend.tsx
+++ packages/themes/src/aiedu/introduction/aiedu-trend/trend.tsx
@@ -1,14 +1,18 @@
-import { cn } from '@oe/ui';
+import { cn } from "@oe/ui";
 
-import { Image } from '@oe/ui';
-import { useTranslations } from 'next-intl';
-import type { SectionComponent } from '../../../_types/theme-page';
+import { Image } from "@oe/ui";
+import { useTranslations } from "next-intl";
+import type { SectionComponent } from "../../../_types/theme-page";
 
-import AIGlobalBg from '@oe/assets/images/theme/aiedu/global.png';
-import { Title, type TitleProps } from '../../_components/title';
+import AIGlobalBg from "@oe/assets/images/theme/aiedu/global.png";
+import { Title, type TitleProps } from "../../_components/title";
 
-import { AieduLayoutSection } from '../../_components/layout-section';
-import { TrendCard, type TrendCardProps, type TrendColorType } from '../../_components/trend-card';
+import { AieduLayoutSection } from "../../_components/layout-section";
+import {
+  TrendCard,
+  type TrendCardProps,
+  type TrendColorType,
+} from "../../_components/trend-card";
 
 export interface AieduIntroductionTrendProps extends TitleProps {
   feature1?: TrendCardProps;
@@ -16,33 +20,36 @@ export interface AieduIntroductionTrendProps extends TitleProps {
   feature3?: TrendCardProps;
 }
 
-const AieduIntroductionTrend: SectionComponent<'introduction', 'aieduTrend'> = ({ props, className }) => {
-  const t = useTranslations('themePage.aiedu.introduction.aieduTrend');
+const AieduIntroductionTrend: SectionComponent<
+  "introduction",
+  "aieduTrend"
+> = ({ props, className }) => {
+  const t = useTranslations("themePage.aiedu.introduction.aieduTrend");
 
   const features = [
     {
-      color: props?.feature1?.color || 'secondary',
-      title: t('feature1.title'),
-      highlight: t('feature1.highlight'),
-      desc: t('feature1.desc'),
+      color: props?.feature1?.color || "secondary",
+      title: t("feature1.title"),
+      highlight: t("feature1.highlight"),
+      desc: t("feature1.desc"),
     },
     {
-      color: props?.feature2?.color || 'warning',
-      title: t('feature2.title'),
-      highlight: t('feature2.highlight'),
-      desc: t('feature2.desc'),
+      color: props?.feature2?.color || "warning",
+      title: t("feature2.title"),
+      highlight: t("feature2.highlight"),
+      desc: t("feature2.desc"),
     },
     {
-      color: props?.feature3?.color || 'info',
-      title: t('feature3.title'),
-      highlight: t('feature3.highlight'),
-      desc: t('feature3.desc'),
+      color: props?.feature3?.color || "info",
+      title: t("feature3.title"),
+      highlight: t("feature3.highlight"),
+      desc: t("feature3.desc"),
     },
   ];
 
   return (
     <AieduLayoutSection
-      className={cn('relative z-1 space-y-8 py-0 md:py-0 lg:py-0', className)}
+      className={cn("relative z-1 space-y-8 py-0 md:py-0 lg:py-0", className)}
       background="bg-gradient-to-b from-info/20 from-50% to-white to-98%"
     >
       <div className="-z-1 absolute h-full w-full">
@@ -56,7 +63,10 @@ const AieduIntroductionTrend: SectionComponent<'introduction', 'aieduTrend'> = (
         />
       </div>
 
-      <Title title={t('title')} className="mx-auto max-w-3xl pt-12 text-center md:pt-16 lg:pt-20" />
+      <Title
+        title={t("title")}
+        className="mx-auto max-w-3xl pt-12 text-center md:pt-16 lg:pt-20"
+      />
       <div className="space-y-4 pb-12 lg:flex lg:items-center lg:gap-8 lg:space-y-0 lg:pb-16 lg:pb-20">
         {features?.map((feature, index) => (
           <TrendCard
