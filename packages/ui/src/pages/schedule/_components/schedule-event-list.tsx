fbe2446c8c4fcf6eab717599fd1bef46e60abd11
Duc Vuong <ducva@gfigroup.io>
2025-05-07 16:28:58 +0700

    fix: add log

--- packages/ui/src/pages/schedule/_components/schedule-event-list.tsx
+++ packages/ui/src/pages/schedule/_components/schedule-event-list.tsx
@@ -1,27 +1,32 @@
-import { getSchedulesEventService } from '@oe/api';
-import { MedalStar } from '@oe/assets';
-import { formatDateTime } from '@oe/core';
-import { Clock, MapPin } from 'lucide-react';
-import { getTranslations } from 'next-intl/server';
-import { Link } from '#common/navigation';
-import { buttonVariants } from '#shadcn/button';
-import ScheduleNoData from './schedule-no-data';
-import { ScheduleEventListPagination } from './schedule-pagination';
+import { getSchedulesEventService } from "@oe/api";
+import { MedalStar } from "@oe/assets";
+import { formatDateTime } from "@oe/core";
+import { Clock, MapPin } from "lucide-react";
+import { getTranslations } from "next-intl/server";
+import { Link } from "#common/navigation";
+import { buttonVariants } from "#shadcn/button";
+import ScheduleNoData from "./schedule-no-data";
+import { ScheduleEventListPagination } from "./schedule-pagination";
 
 export default async function ScheduleEventList({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
-  const t = await getTranslations('schedule.website');
-  const { search = '', page = 1, start_date = Date.now(), end_date = '' } = await searchParams;
+  const t = await getTranslations("schedule.website");
+  const {
+    search = "",
+    page = 1,
+    start_date = Date.now(),
+    end_date = "",
+  } = await searchParams;
 
   const eventParams = {
     page: Number(page),
     per_page: 10,
     search_term: search,
-    search_categories: 'name',
-    sort: 'create_at desc',
+    search_categories: "name",
+    sort: "create_at desc",
     end_at_lte: end_date ? Number(end_date) : undefined,
     start_at_gte: start_date ? Number(start_date) : undefined,
   };
@@ -30,28 +35,30 @@ export default async function ScheduleEventList({
     params: eventParams,
   });
 
-  console.log('eventsData', eventsData);
-
   return (
     <>
       <div className="mb-4 flex items-center gap-2">
         <MedalStar color="#222222" width={24} height={24} />
-        <h3 className="mcaption-semibold20 mb-0">{t('upcomingEvents')}</h3>
+        <h3 className="mcaption-semibold20 mb-0">{t("upcomingEvents")}</h3>
       </div>
       <div className="flex flex-col gap-4 text-left">
         {eventsData?.results && eventsData.results.length > 0 ? (
-          eventsData.results.map(event => (
+          eventsData.results.map((event) => (
             <div
               key={event.id}
               className="flex flex-col justify-start rounded-[16px] border border-neutral-100 p-5 shadow-sm"
             >
-              <h3 className="mcaption-semibold20 mb-4 text-primary">{event.name}</h3>
+              <h3 className="mcaption-semibold20 mb-4 text-primary">
+                {event.name}
+              </h3>
               <p className="mb-4">{event.description}</p>
 
               {event.start_at && (
                 <div className="mb-2 flex items-center gap-2">
                   <Clock className="h-6 w-6 text-primary" />
-                  <span className="mcaption-semibold16">{formatDateTime(event.start_at)}</span>
+                  <span className="mcaption-semibold16">
+                    {formatDateTime(event.start_at)}
+                  </span>
                 </div>
               )}
 
@@ -63,8 +70,12 @@ export default async function ScheduleEventList({
               )}
 
               <div className="flex justify-end gap-3">
-                <Link href={event.join_link} target="_blank" className={buttonVariants({ variant: 'default' })}>
-                  {t('joinNow')}
+                <Link
+                  href={event.join_link}
+                  target="_blank"
+                  className={buttonVariants({ variant: "default" })}
+                >
+                  {t("joinNow")}
                 </Link>
               </div>
             </div>
@@ -73,7 +84,10 @@ export default async function ScheduleEventList({
           <ScheduleNoData />
         )}
       </div>
-      <ScheduleEventListPagination events={eventsData} pageValue={Number(page)} />
+      <ScheduleEventListPagination
+        events={eventsData}
+        pageValue={Number(page)}
+      />
     </>
   );
 }
