803f3ee661f66d8630d32c68e679678a1c9fe98a
phuongle-vbi <phuongltk@gfigroup.io>
2025-05-09 11:10:41 +0700

    feat: required whole number for duration and study load field #369

--- packages/dashboard/src/creator/courses/ai-course/_components/form-field/duration-field.tsx
+++ packages/dashboard/src/creator/courses/ai-course/_components/form-field/duration-field.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { ICreateAICourseOutline } from '@oe/api';
-import { Autocomplete } from '@oe/ui';
+import { Autocomplete, cn } from '@oe/ui';
 import { InputNumber } from '@oe/ui';
 import { FormFieldWithLabel } from '@oe/ui';
 import { useTranslations } from 'next-intl';
@@ -16,28 +16,23 @@ export function DurationField({
 
   return (
     <>
-      <p className="mbutton-semibold16 mb-2">
+      <p className={cn("mbutton-semibold16 mb-2", !!form.formState.errors.duration && 'text-destructive')}>
         {tAICourseForm('courseDuration')}
         <span className="ml-1 text-destructive">*</span>
       </p>
-      <div className="flex rounded border">
+      <div className="flex rounded">
         <FormFieldWithLabel name="duration_type" className="min-w-24">
           <Autocomplete
             options={['day', 'week']}
             getOptionLabel={val => tAICourseForm(val)}
-            triggerProps={{ className: 'bg-muted border-0 rounded-r-none' }}
+            triggerProps={{ className: 'bg-muted border-r-0 rounded-r-none' }}
           />
         </FormFieldWithLabel>
 
         <FormFieldWithLabel name="duration" className="grow">
-          <InputNumber min={1} className="!border-0 flex-1 rounded-r-radius-m" />
+          <InputNumber min={1} step={1} className="flex-1 rounded-r-radius-m rounded-l-none" />
         </FormFieldWithLabel>
       </div>
-      {form.formState.errors.duration && (
-        <p className="mcaption-regular16 !mt-3 text-negative">
-          {tAICourseForm(form.formState.errors.duration?.message as string)}
-        </p>
-      )}
     </>
   );
 }
