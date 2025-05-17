b403879b475ebc83224dfc479a344d127a9b04d6
thienlinh <thienlinhus@gmail.com>
2025-05-17 10:11:51 +0700

    chore: update github action deployment workflow

--- apps/platform/sst.config.openedutest.ts
+++ apps/platform/sst.config.openedutest.ts
@@ -12,7 +12,7 @@ export default $config({
   // biome-ignore lint/suspicious/useAwait: <explanation>
   async run() {
     const AUTH_SECRET = process.env.AUTH_SECRET || '';
-    console.log('AUTH_SECRET in sst.config.openedutest.ts', AUTH_SECRET);
+    console.log('AUTH_SECRET in sst.config.openedutest.ts', AUTH_SECRET?.length);
     new sst.aws.Nextjs('openedutest', {
       // path: 'apps/platform',
       // router: {
